89fbeadbbf4d144fe35daf16f3efcaf2601b5156
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-09-21 20:01:12 +0200

    Add missing submodules.

--- buildconf.py
+++ buildconf.py
@@ -3,6 +3,17 @@ MODULES = [
 	"kde-extra-cmake-modules",
 	"kde-solid",
 	"vibe",
+	"widget-styles",
+	"icon-themes",
+	"wallpapers",
+	"loginmanager",
+	"greenisland",
+	"swordfish",
+	"system-preferences",
+	"archiver",
+	"eyesight",
+	"terminal",
+	"widget-factory",
 ]
 
 # Dependencies
@@ -10,4 +21,15 @@ DEPENDENCIES = {
 	"kde-extra-cmake-modules": "",
 	"kde-solid": "",
 	"vibe": "kde-solid",
+	"widget-styles": "vibe",
+	"icon-themes": "",
+	"wallpapers": "",
+	"loginmanager": "",
+	"greenisland": "vibe,wallpapers",
+	"swordfish": "vibe",
+	"system-preferences": "vibe",
+	"archiver": "vibe",
+	"eyesight": "vibe",
+	"terminal": "vibe",
+	"widget-factory": "widget-styles",
 }
